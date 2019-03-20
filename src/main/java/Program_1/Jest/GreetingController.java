e6b1dbb723efac02f4aa81487153b561d7aae46d
Maxim Vaslyaev <jest1bmc@gmail.com>
2019-03-20 21:05:31 +0300

    устранена ошибка messageRepo

--- src/main/java/Program_1/Jest/GreetingController.java
+++ src/main/java/Program_1/Jest/GreetingController.java
@@ -30,7 +30,7 @@ private MessageRepo messageRepo;
     public String add(@RequestParam String text,@RequestParam String tag,Map<String,Object> model)
     {
       Message message =new Message(text,tag);
-      MessageRepo.save(message);
+      messageRepo.save(message);
       Iterable<Message>messages=messageRepo.findAll();
         model.put("messages","messages");
         return "clock";
