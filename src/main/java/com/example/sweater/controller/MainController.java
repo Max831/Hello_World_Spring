a0ed4b7e8df5dfb2a36413fce5758f19c0a9989d
Maxim Vaslyaev <jest1bmc@gmail.com>
2019-03-26 13:31:51 +0300

    добавлена авторизация и регистрация

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.sweater;
+package com.example.sweater.controller;
 
 import com.example.sweater.domain.Message;
 import com.example.sweater.repos.MessageRepo;
@@ -13,30 +13,24 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
     //@GetMapping указывает, что данный метод обрабатывает GET-запрос, а также задаёт дополнительный сегмент адреса.
-    @GetMapping("/greeting")
+    @GetMapping("/")
     //@RequestMapping задаёт базовый адрес, по которому можно обращаться к целевым методам,
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
-        model.put("name", name);
+    public String greeting( Map<String, Object> model) {
         return "greeting";
     }
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String,Object> model){
         Iterable<Message> messages=messageRepo.findAll();
 
         model.put("messages",messages);
         return "main";
     }
-    @GetMapping("/good")
-    public String good(Map<String,Object> mimi){
-        mimi.put("qw","Goodbye");
-        return "good";
-    }
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String,Object> model){
         Message message=new Message(text,tag);
         messageRepo.save(message);
